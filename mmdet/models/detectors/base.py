fix w/h normalization in onnx export

mmdet/models/detectors/base.py
@@ -172,7 +172,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
                 std = torch.tensor(normalize_cfg['std'])[..., None, None]
                 onnx_input = (img[0] - mean) / std
             onnx_output = self.onnx_export(onnx_input, img_metas[0])
-            onnx_w, onnx_h = img_metas[0][0]['img_shape_for_onnx']
+            onnx_h, onnx_w = img_metas[0][0]['img_shape_for_onnx']
             onnx_output[0][:,:,:4] = onnx_output[0][:,:,:4] / torch.stack([onnx_w, onnx_h, onnx_w, onnx_h])
             return onnx_output
 